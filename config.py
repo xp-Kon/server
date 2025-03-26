5c9eb8193ecfdbcf9d6db1b8613699795ba392c3
xp-Kon <85104992+xp-Kon@users.noreply.github.com>
2025-03-26 15:29:25 +0000

    符合规范

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
 MAIL_SERVER = 'smtp.qq.com'
 MAIL_PORT = 465
 MAIL_USE_SSL = True
-MAIL_USERNAME = '你的QQ邮箱@qq.com'
-MAIL_PASSWORD = '你的SMTP授权码'
-TARGET_EMAIL = '接收订单的邮箱@qq.com'
+MAIL_USERNAME = '1374198709@qq.com'
+MAIL_PASSWORD = 'hbcydrfoszmngjgd'
+TARGET_EMAIL = '1440138181@qq.com'
 
 SQLALCHEMY_DATABASE_URI = 'sqlite:///menu.db'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
